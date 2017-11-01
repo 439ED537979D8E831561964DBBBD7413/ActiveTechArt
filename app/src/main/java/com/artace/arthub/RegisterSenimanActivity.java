2a35ced83463028832a72ba945bccd7ddbf59d03
Aditya Herlambang <herlambang.aditya456@gmail.com>
2017-11-01 20:10:46 +0800

    Add Register Activity (2)

---
+++ app/src/main/java/com/artace/arthub/RegisterSenimanActivity.java
@@ -0,0 +1,34 @@
+package com.artace.arthub;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
+
+public class RegisterSenimanActivity extends AppCompatActivity {
+
+    EditText password;
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_seniman);
+
+        //START : TOOLBAR
+
+        mToolbar = (Toolbar) findViewById(R.id.register_seniman_toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setTitle("Register Seniman");
+
+        //END : TOOLBAR
+
+        password = (EditText) findViewById(R.id.register_seniman_password);
+        password.setTransformationMethod(new PasswordTransformationMethod());
+
+    }
+}
