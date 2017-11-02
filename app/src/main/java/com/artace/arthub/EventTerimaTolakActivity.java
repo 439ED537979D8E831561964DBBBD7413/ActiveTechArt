4e5efb1333af143634b8907340a71481d271eaba
anandadwi45 <anandadwi45@gmail.com>
2017-11-02 22:09:13 +0800

    Last push 11/02/17

---
+++ app/src/main/java/com/artace/arthub/EventTerimaTolakActivity.java
@@ -0,0 +1,13 @@
+package com.artace.arthub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventTerimaTolakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_terima_tolak);
+    }
+}
