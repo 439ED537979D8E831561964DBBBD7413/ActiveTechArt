cecf2be103ec85d16f5f0730fb898d2380996901
USER <kadekarik45@gmail.com>
2017-11-02 22:07:48 -0500

    last push

---
+++ app/src/main/java/com/artace/arthub/EoSenimanActivity.java
@@ -0,0 +1,13 @@
+package com.artace.arthub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EoSenimanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eo_seniman);
+    }
+}
--- app/src/main/java/com/artace/arthub/SenimanHomeProfileActivity.java
+++ app/src/main/java/com/artace/arthub/SenimanHomeProfileActivity.java
@@ -3,11 +3,11 @@ package com.artace.arthub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SenimanListTawaran extends AppCompatActivity {
+public class SenimanHomeProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seniman_list_tawaran);
+        setContentView(R.layout.activity_seniman_home_profile);
     }
 }
