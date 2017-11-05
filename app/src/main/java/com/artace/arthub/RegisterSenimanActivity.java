54f8cd3ecf44d58548bb76d72ddc0c3c89b464ea
USER <kadekarik45@gmail.com>
2017-11-04 23:12:31 -0500

    push

--- app/src/main/java/com/artace/arthub/RegisterSenimanActivity.java
+++ app/src/main/java/com/artace/arthub/RegisterSenimanActivity.java
@@ -1,34 +1,171 @@
 package com.artace.arthub;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.artace.arthub.connection.DatabaseConnection;
+import com.artace.arthub.controller.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RegisterSenimanActivity extends AppCompatActivity {
 
-    EditText password;
+    Spinner jenis_seniman;
+    EditText username, password, nama, keahlian;
+    RadioButton laki, perempuan;
+    Button portofolio, foto, daftar;
+    RequestQueue queue;
+    ImageView view_foto;
     Toolbar mToolbar;
+    String[] listJenisSeniman;
+
+    Bitmap bitmap, decoded;
+    int success;
+    int PICK_IMAGE_REQUEST = 1;
+    int bitmap_size = 60; // range 1 - 100
+    ConnectivityManager conMgr;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String UPLOAD_URL = DatabaseConnection.INSERT_REGISTER_SENIMAN;
+    String READ_JENIS_SENIMAN_URL;
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+
+    String tag_json_obj = "json_obj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_seniman);
-
         //START : TOOLBAR
 
-//        mToolbar = (Toolbar) findViewById(R.id.register_seniman_toolbar);
-//        setSupportActionBar(mToolbar);
-//        ActionBar ab = getSupportActionBar();
-//        ab.setDisplayHomeAsUpEnabled(true);
-//        ab.setTitle("Register Seniman");
+        READ_JENIS_SENIMAN_URL = DatabaseConnection.getReadJenisSeniman();
+
+        mToolbar = (Toolbar) findViewById(R.id.register_seniman_toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setTitle("Register Seniman");
 
         //END : TOOLBAR
+        //cek connection
+        conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        {
+            if (conMgr.getActiveNetworkInfo() != null
+                    && conMgr.getActiveNetworkInfo().isAvailable()
+                    && conMgr.getActiveNetworkInfo().isConnected()) {
+            } else {
+                Toast.makeText(getApplicationContext(), "No Internet Connection",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        //end cek
 
-        //password = (EditText) findViewById(R.id.register_seniman_password);
-        //password.setTransformationMethod(new PasswordTransformationMethod());
+        jenis_seniman = (Spinner) findViewById(R.id.register_seniman_jenis);
+        username = (EditText) findViewById(R.id.register_seniman_username);
+        password = (EditText) findViewById(R.id.register_seniman_password);
+        nama = (EditText) findViewById(R.id.register_seniman_nama);
+        keahlian = (EditText) findViewById(R.id.register_seniman_keahlian);
+        laki = (RadioButton) findViewById(R.id.register_seniman_laki);
+        perempuan = (RadioButton) findViewById(R.id.register_seniman_perempuan);
+        portofolio = (Button) findViewById(R.id.register_seniman_btnPortfolio);
+        foto = (Button) findViewById(R.id.register_seniman_btnFoto);
+        view_foto = (ImageView) findViewById(R.id.register_seniman_viewFoto);
+        daftar = (Button) findViewById(R.id.register_choose_seniman);
 
+        ReadJenisSeniman();
+
+    }
+
+    private void ReadJenisSeniman() {
+
+        Log.e("man","uhuy");
+
+        final Context contextFinal = this;
+        queue = AppController.getInstance().getRequestQueue();
+
+        JsonArrayRequest readRequest = new JsonArrayRequest(READ_JENIS_SENIMAN_URL, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+                try {
+                    JSONArray jr = response.getJSONArray(0);
+                    List<Integer> listValue = new ArrayList<Integer>();
+                    List<String> list = new ArrayList<String>();
+
+
+                    for (int i = 0; i < jr.length(); i++) {
+                        try {
+
+                            JSONObject obj = (JSONObject) jr.get(i);
+                            listValue.add(obj.getInt("id_jenis_seniman"));
+
+                            list.add(obj.getString("jenis_seniman"));
+
+                            Log.e("man","tapppjiwaaa");
+
+
+                        } catch (Exception e) {
+                            Log.e("man","LOG gamao! = " + e.getMessage());
+                        }
+                    }
+                    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(contextFinal,android.R.layout.simple_spinner_dropdown_item, list);
+
+                    jenis_seniman.setAdapter(dataAdapter);
+                } catch (Exception e) {
+                    Log.e("man","LOG gamao diluar! = " + e.getMessage());
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+            }
+        });
+
+        // Adding request to request queue
+//        AppController.getInstance().addToRequestQueue(readRequest, tag_json_obj);
+        queue.add(readRequest);
     }
 }
--- app/src/main/java/com/artace/arthub/connection/DatabaseConnection.java
+++ app/src/main/java/com/artace/arthub/connection/DatabaseConnection.java
@@ -4,15 +4,24 @@ package com.artace.arthub.connection;
 public class DatabaseConnection {
 
     public static String BASE_URL = "http://arthubdevelopment.000webhostapp.com/";
-//    public static String BASE_URL = "http://192.168.43.206/arthub/";
+
     public static String READ_EVENTORGANIZER_EVENTS = BASE_URL+"eventorganizer/EventOrganizerEvents.php";
     public static String READ_EVENT_DETAIL = BASE_URL+"event/DetailEvent.php";
     public static String CREATE_EVENT_ORGANIZER = BASE_URL+"eventorganizer/CreateEventOrganizer.php";
     //DELETE
     public static String DELETE_EVENT = BASE_URL+"event/DeleteEvent.php";
+    //LOGIN
     public static String LOGIN = BASE_URL+"login/Login.php";
+    //READ JENIS SENIMAN
+    public  static String READ_JENIS_SENIMAN = BASE_URL+"jenis_seniman/JenisSeniman.php";
+    //INSERT REGISTER SENIMAN
+    public  static String INSERT_REGISTER_SENIMAN = BASE_URL+"seniman/RegisterSeniman.php";
 
     public DatabaseConnection() {
+    }
+
+    public static String getReadJenisSeniman() {
+        return READ_JENIS_SENIMAN;
     }
 
     public static String getDeleteEvent(int id_event) {
